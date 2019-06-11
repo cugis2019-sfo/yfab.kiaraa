17cb221fe7505673a9781121fd4d16bd3f70cfae
kilampkinss <51681190+kilampkinss@users.noreply.github.com>
2019-06-11 10:43:12 -0700

    Add files via upload

--- Kiara STEM.py
+++ Kiara STEM.py
@@ -5,34 +5,3 @@ Created on Mon Jun 10 14:07:32 2019
 @author: admin
 """
 
-5/8 #division 
-print("Hello" ,5, "World") 
-print("I am learning to code in python today")
-print(hey)
-print("5")
-5*2 #multiplicaton
-5/2 #division
-5-2 #subtraction
-5**2 #exponents
-(8/9)*3 #8 divided by 9 then multiplied by 3
-
-#writing code to sum any two numbers
-def sum(a,b):
-    sum = a + b
-    print("the sum is",sum)
-    
-sum(4, 5)
-
-#writing code to sum any three numbers
-def sum(a,b,c):
-    sum = a + b + c
-    print("the sum is" ,sum)
-    
-sum(3, 9, 6)
- 
-#writing code to sum up a triangle
-def trianglearea(b, h):
-    trianglearea = b*h*0.5
-    print("the area of a triangle is", trianglearea)
-    
-trianglearea(3*3)
