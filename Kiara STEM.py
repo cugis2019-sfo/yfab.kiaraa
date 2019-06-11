a15b2cad770dc386678d726d90de7336beffa18c
kilampkinss <51681190+kilampkinss@users.noreply.github.com>
2019-06-11 15:26:58 -0700

    Add files via upload

--- Kiara STEM.py
+++ Kiara STEM.py
@@ -5,3 +5,70 @@ Created on Mon Jun 10 14:07:32 2019
 @author: admin
 """
 
+5/8 #division 
+print("Hello" ,5, "World") 
+print("I am learning to code in python today")
+print(hey)
+print("5")
+5*2 #multiplicaton
+5/2 #division
+5-2 #subtraction
+5**2 #exponents
+(8/9)*3 #8 divided by 9 then multiplied by 3
+
+#writing code to sum any two numbers
+def sum(a,b):
+    sum = a + b
+    print("the sum is",sum)
+    
+sum(4, 5)
+#writing code to sum any three numbers
+def sum(a,b,c):
+    sum = a + b + c
+    print("the sum is" ,sum)
+    
+sum(3, 9, 6)
+ 
+#writing code to sum up a triangle
+def trianglearea(b, h):
+    trianglearea = b*h*0.5
+    print("the area of a triangle is", trianglearea)
+    
+trianglearea(3*3)
+
+#greeting
+def greeting(b):
+    print("hello" , b)
+greeting("Kiara")
+
+import math
+dir(math)
+pi
+math.pi
+math.e
+math.sqrt
+math.sqrt(2)
+math.factorial(1730)
+math.pow(3,10)
+math.remainder(4,6)
+math.remainder(10,3000)
+
+#calculating cubicroot of any variable
+def cube(a):
+    cube = a
+    print(math.pow(a,(1/3)))
+    
+cube(12)
+
+#ask user for number and assign it for number
+def user():
+    print("please select a number")
+def user():
+    print("please select a number for k")
+k = 16    
+
+def useranswer(k):
+    print(math.pow(k,(1/3)))
+    
+useranswer(k)
+    
